4ba91f9f3dc63cd0c8afe99469e23a4672225953
cch5ng <cch5ng@gmail.com>
2016-01-09 18:42:54 -0800

    fix: removed insecure package. implemented methods for collection write functions chore: cleaned formatting for lint warnings doc: added comments for where code was updated per task

--- server/main.js
+++ server/main.js
@@ -4,14 +4,25 @@
 // users have the username 'user1@test.com' .. 'user8@test.com'
 // and the password test123 
 
-  Meteor.startup(function () {
-    if (!Meteor.users.findOne()){
-      for (var i=1;i<9;i++){
-        var email = "user"+i+"@test.com";
-        var username = "user"+i;
-        var avatar = "ava"+i+".png";
-        console.log("creating a user with password 'test123' and username/ email: "+email);
-        Meteor.users.insert({profile:{username:username, avatar:avatar}, emails:[{address:email}],services:{ password:{"bcrypt" : "$2a$10$I3erQ084OiyILTv8ybtQ4ON6wusgPbMZ6.P33zzSDei.BbDL.Q4EO"}}});
-      }
-    }
-  });
+Meteor.startup(function () {
+	if (!Meteor.users.findOne()){
+		for (var i=1;i<9;i++){
+			var email = "user"+i+"@test.com";
+			var username = "user"+i;
+			var avatar = "ava"+i+".png";
+			console.log("creating a user with password 'test123' and username/ email: "+email);
+			Meteor.users.insert({profile:{username:username, avatar:avatar}, emails:[{address:email}],services:{ password:{"bcrypt" : "$2a$10$I3erQ084OiyILTv8ybtQ4ON6wusgPbMZ6.P33zzSDei.BbDL.Q4EO"}}});
+		}
+	}
+});
+
+//TASK2
+Meteor.methods({
+	addChat: function(user1Id, user2Id) {
+		return Chats.insert({user1Id: user1Id, user2Id: user2Id});
+	},
+	updateChat: function(chatid, chat) {
+		Chats.update(chatid, chat);
+//		console.log('attempt update chat');
+	}
+});
